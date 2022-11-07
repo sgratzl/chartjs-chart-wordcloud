9e519082227312087cb29d0e376ea05ba8136444
Samuel Gratzl <sam@sgratzl.com>
2022-11-07 10:56:59 -0500

    build: upgrade

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,6 @@
 /* eslint-env node */
-
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
-  rootDir: './src',
-  testRegex: '((\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '((\\.|/)(test|spec))\\.m?tsx?$',
 };
